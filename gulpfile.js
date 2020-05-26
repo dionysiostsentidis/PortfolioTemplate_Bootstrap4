af1f7a6bcbdcee759919d01b876e9189e2f7001f
dionweb <dionysiostsentidis@gmail.com>
2020-05-27 01:41:02 +0200

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,55 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
 
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+function sassify() {
+    return gulp
+        .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
-});
+}
 
 // Move JS Files to src/js
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+function js() {
+    return gulp
+        .src([
+            "node_modules/bootstrap/dist/js/bootstrap.min.js",
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/popper.js/dist/umd/popper.min.js",
+        ])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
-});
+}
 
 // Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
-
+function serve() {
     browserSync.init({
-        server: "./src"  
+        server: "./src",
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+    gulp.watch("node_modules/bootstrap/scss/bootstrap.scss", sassify);
+    gulp.watch("src/scss/*.scss", sassify);
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+}
 
 // Move Fonts to src/fonts
-gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
-})
+function fonts() {
+    return gulp
+        .src("node_modules/font-awesome/fonts/*")
+        .pipe(gulp.dest("src/fonts"));
+}
 
 // Move Font Awesome CSS to src/css
-gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
-})
-
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+function fa() {
+    return gulp
+        .src("node_modules/font-awesome/css/font-awesome.min.css")
+        .pipe(gulp.dest("src/css"));
+}
+exports.sassify = sassify;
+exports.js = js;
+exports.serve = serve;
+exports.fa = fa;
+exports.fonts = fonts;
+gulp.task("default", gulp.series(sassify, js, fonts, fa, serve));
